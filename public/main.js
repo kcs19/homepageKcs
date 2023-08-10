3080de2b2d7e3054adc0388b2aed08c737657db5
kcs19 <changsung19@gmail.com>
2023-08-11 02:08:37 +0900

    시간 지나면 메인 화면 넘기기, 문구 넘기기

--- public/main.js
+++ public/main.js
@@ -19,7 +19,6 @@ myEmartBtn.addEventListener('click', function () {
 });
 
 const login_inputs = document.querySelectorAll('#head > ul > li > a > span');
-
 for (const login_input of login_inputs) {
     login_input.addEventListener('mouseover', function (e) {
         login_input.setAttribute('class', 'mouseover');
@@ -28,3 +27,70 @@ for (const login_input of login_inputs) {
         login_input.setAttribute('class', '');
     });
 }// 아이디 밑줄
+
+const right_btn = document.querySelector('#right_button');
+const left_btn = document.getElementById('left_button');
+
+left_btn.addEventListener('click', changePreviousPic);
+right_btn.addEventListener('click', changePic);
+var interval = setInterval(changePic, 3000);
+
+function changePic() {
+    let select_point = document.querySelector('.select_point');
+    let show = document.querySelector('.show');
+    if (select_point.nextElementSibling) {
+        select_point.nextElementSibling.classList.add('select_point');
+        show.nextElementSibling.classList.add('show');
+    } else {
+        select_point.parentElement.firstElementChild.classList.add('select_point');
+        show.parentElement.firstElementChild.classList.add('show');
+    }
+    show.classList.remove('show');
+    select_point.classList.remove('select_point');
+}
+function changePreviousPic(e) {
+    if (e.target.classList.contains('show')) {
+        console.log("dfkasjsdf");
+    }
+    let select_point1 = document.querySelector('.select_point');
+    let show1 = document.querySelector('.show');
+    console.log("ddd");
+    if (select_point1.previousElementSibling) {
+        console.log("왼쪽");
+        select_point1.previousElementSibling.classList.add('select_point');
+        show1.previousElementSibling.classList.add('show');
+    } else {
+        console.log("더 왼쪽");
+        select_point1.parentElement.lastElementChild.classList.add('select_point');
+        show1.parentElement.lastElementChild.classList.add('show');
+    }
+    show1.classList.remove('show');
+    select_point1.classList.remove('select_point');
+}
+
+const main_stop_btn = document.querySelector('#stop');
+main_stop_btn.addEventListener('click', function (e) {
+    if (interval) {
+        main_stop_btn.style.backgroundPosition = "0 -57px";
+        clearInterval(interval);
+        interval = null;
+    }
+    else {
+        main_stop_btn.style.backgroundPosition = "0 -28px";
+        interval = setInterval(changePic, 3000);
+    }
+});
+
+let decrease = 0;
+const slide_bar = document.querySelector('#slide');
+function slide() {
+    decrease -= 10
+    slide_bar.style.left = decrease + "px";
+
+    // const firstChild = slide_bar.firstElementChild;
+    // if (parseInt(slide_bar.style.left) == -500) {
+    //     slide_bar.removeChild(firstChild);
+    //     slide_bar.appendChild(firstChild);
+    // }
+}
+setInterval(slide, 100);
